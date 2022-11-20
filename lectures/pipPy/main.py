309d2146699f3014e37de1b65ef307479e5856d2
Lera <valeriiakolesnikova21@gmail.com>
2022-11-20 14:48:29 +0300

    lection 5 added

---
+++ lectures/pipPy/main.py
@@ -0,0 +1,42 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+app = ApplicationBuilder().token(
+    "5847032200:AAF3w2uSc41AIbaqUvB2XYSuz2NzufY15WE").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+app.run_polling()
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# list = [1, 2, 3, 2, 7]
+# plt.plot(list)
+# plt.show()
+
+
+# import emoji
+
+# print(emoji.emojize('Python is :thumbs_up:'))
+
+
+# from progress.bar import Bar
+# import time
+
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(1)
+#     bar.next()
+# bar.finish()
+
+# from isOdd import isOdd
+
+# print(isOdd(0))
+# print(isOdd(5))
